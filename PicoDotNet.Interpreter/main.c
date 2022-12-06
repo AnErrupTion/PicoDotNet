5fe0f74809178f0cb4cbe7fcc68db0615111731d
AnErrupTion <anerruption@disroot.org>
2022-12-06 19:56:27 +0100

    More interpreter work

--- PicoDotNet.Interpreter/main.c
+++ PicoDotNet.Interpreter/main.c
@@ -5,10 +5,12 @@
 #include <stdarg.h>
 #include <string.h>
 #include <ctype.h>
+#include <locale.h>
 
 typedef uint8_t byte;
 typedef uint16_t ushort;
 typedef uint32_t uint;
+typedef uint64_t ulong;
 
 typedef uint8_t BYTE;
 typedef uint16_t WORD;
@@ -55,6 +57,22 @@ typedef struct _IMAGE_DATA_DIRECTORY {
     DWORD Size;
 } IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY;
 
+typedef struct _IMAGE_HEADER_SECTION {
+    BYTE  Name[8];
+    union {
+        DWORD PhysicalAddress;
+        DWORD VirtualSize;
+    } Misc;
+    DWORD VirtualAddress;
+    DWORD SizeOfRawData;
+    DWORD PointerToRawData;
+    DWORD PointerToRelocations;
+    DWORD PointerToLinenumbers;
+    WORD  NumberOfRelocations;
+    WORD  NumberOfLinenumbers;
+    DWORD Characteristics;
+} IMAGE_HEADER_SECTION, *PIMAGE_HEADER_SECTION;
+
 typedef struct _IMAGE_OPTIONAL_HEADER {
     WORD                 Magic;
     BYTE                 MajorLinkerVersion;
@@ -87,20 +105,8 @@ typedef struct _IMAGE_OPTIONAL_HEADER {
     DWORD                LoaderFlags;
     DWORD                NumberOfRvaAndSizes;
     IMAGE_DATA_DIRECTORY DataDirectory[16];
+    IMAGE_HEADER_SECTION Sections[3];
 } IMAGE_OPTIONAL_HEADER32, *PIMAGE_OPTIONAL_HEADER32;
-
-typedef struct _IMAGE_HEADER_SECTION {
-    BYTE  Name[8];
-    DWORD VirtualSize;
-    DWORD VirtualAddress;
-    DWORD SizeOfRawData;
-    DWORD PointerToRawData;
-    DWORD PointerToRelocations;
-    DWORD PointerToLinenumbers;
-    WORD  NumberOfRelocations;
-    WORD  NumberOfLinenumbers;
-    DWORD Characteristics;
-} IMAGE_HEADER_SECTION, *PIMAGE_HEADER_SECTION;
 
 typedef struct _IMAGE_CLR_HEADER {
     DWORD HeaderSize;
@@ -140,7 +146,9 @@ byte* ReadBytes(const char* name) {
     return dstBuf;
 }
 
-byte* GetBuffer(byte* buffer, uint offset, uint size) {
+byte* buffer;
+
+byte* GetBuffer(uint offset, uint size) {
     byte* buf = malloc(size);
 
     for (int i = 0; i < size; i++)
@@ -149,34 +157,87 @@ byte* GetBuffer(byte* buffer, uint offset, uint size) {
     return buf;
 }
 
+ushort number_of_sections;
+IMAGE_HEADER_SECTION* sections;
+
+ulong RvaToOffset(ulong rva) {
+    if (rva == 0) {
+        printf("RVA cannot be 0!\n");
+        exit(1);
+    }
+
+    for (ushort i = 0; i < number_of_sections; i++) {
+        IMAGE_HEADER_SECTION section = sections[i];
+        if (section.VirtualAddress <= rva && section.VirtualAddress + section.SizeOfRawData >= rva)
+            return section.PointerToRawData + (rva - section.VirtualAddress);
+    }
+
+    printf("Cannot find the section.\n");
+    exit(1);
+}
+
 int main() {
-    byte* buffer = ReadBytes("TestApp.dll");
+    buffer = ReadBytes("TestApp.dll");
     uint offset = 0;
 
-    IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*) GetBuffer(buffer, offset, sizeof(IMAGE_DOS_HEADER));
+    IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*) GetBuffer(offset, sizeof(IMAGE_DOS_HEADER));
     offset += dos_header->e_lfanew - 1;
 
-    IMAGE_FILE_HEADER* pe_file_header = (IMAGE_FILE_HEADER*) GetBuffer(buffer, offset, sizeof(IMAGE_FILE_HEADER));
+    if (dos_header->e_magic != 0x5A4D) {
+        printf("Invalid DOS header magic!\n");
+        exit(1);
+    }
+
+    IMAGE_FILE_HEADER* pe_file_header = (IMAGE_FILE_HEADER*) GetBuffer(offset, sizeof(IMAGE_FILE_HEADER));
     offset += sizeof(IMAGE_FILE_HEADER);
 
-    IMAGE_OPTIONAL_HEADER32* pe_optional_header = (IMAGE_OPTIONAL_HEADER32*) GetBuffer(buffer, offset, sizeof(IMAGE_OPTIONAL_HEADER32));
+    if (pe_file_header->Signature != 0x4550) {
+        printf("Invalid PE file header signature!\n");
+        exit(1);
+    }
+
+    IMAGE_OPTIONAL_HEADER32* pe_optional_header = (IMAGE_OPTIONAL_HEADER32*) GetBuffer(offset, sizeof(IMAGE_OPTIONAL_HEADER32));
     offset += sizeof(IMAGE_OPTIONAL_HEADER32);
 
-    IMAGE_HEADER_SECTION* sections = (IMAGE_HEADER_SECTION*) GetBuffer(buffer, offset, pe_file_header->NumberOfSections);
-    IMAGE_HEADER_SECTION section = sections[0];
+    if (pe_optional_header->Magic != 0x10B) { // 0x10B is 32-bit, 0x20B is 64-bit
+        printf("Invalid PE optional header magic!\n");
+        exit(1);
+    }
 
-    IMAGE_DATA_DIRECTORY clr_directory_header = pe_optional_header->DataDirectory[15];
+    for (int i = 0; i < 16; i++) {
+        IMAGE_DATA_DIRECTORY directory = pe_optional_header->DataDirectory[i];
+        printf("%d: virt %d sz %d\n", i, directory.VirtualAddress, directory.Size);
+    }
 
-    offset = section.PointerToRawData + (clr_directory_header.VirtualAddress - section.VirtualAddress);
-    byte* clr_directory_data = GetBuffer(buffer, offset, clr_directory_header.Size);
+    number_of_sections = pe_file_header->NumberOfSections;
+    // TODO: This reads incorrectly, why??
+    //sections = (IMAGE_HEADER_SECTION*) GetBuffer(offset, number_of_sections);
+    sections = pe_optional_header->Sections;
 
-    IMAGE_CLR_HEADER* clr_header = (IMAGE_CLR_HEADER*) GetBuffer(clr_directory_data, 0, sizeof(IMAGE_CLR_HEADER));
+    for (ushort i = 0; i < number_of_sections; i++) {
+        IMAGE_HEADER_SECTION section = sections[i];
+        for (int j = 0; j < 8; j++)
+            printf("%c", section.Name[j]);
+        printf("\n");
+    }
 
-    // TODO: Read strong name hash, parse metadata
+    IMAGE_DATA_DIRECTORY clr_directory_header = pe_optional_header->DataDirectory[14];
 
+    // TODO: Why is the size so huge and addr 0???
+    offset = RvaToOffset(clr_directory_header.VirtualAddress);
+    IMAGE_CLR_HEADER* clr_header = (IMAGE_CLR_HEADER*) GetBuffer(offset, clr_directory_header.Size);
+
+    printf("sz %d addr %d maj %d min %d\n", clr_header->HeaderSize, clr_header->StrongNameSignatureAddress, clr_header->MajorRuntimeVersion, clr_header->MinorRuntimeVersion);
+
+    offset = RvaToOffset(clr_header->StrongNameSignatureAddress);
+    printf("%d\n", offset);
+    //byte* strong_name_hash = GetBuffer(offset, clr_header->StrongNameSignatureSize);
+
+    // TODO: Parse metadata
+
+    //free(strong_name_hash);
     free(clr_header);
-    free(clr_directory_data);
-    free(sections);
+    //free(sections);
     free(pe_optional_header);
     free(pe_file_header);
     free(dos_header);
