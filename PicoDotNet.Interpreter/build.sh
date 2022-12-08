3832e0f26e17ae1ecc83d299655768448cd6313b
AnErrupTion <anerruption@disroot.org>
2022-12-08 11:13:24 +0100

    Clean up

--- PicoDotNet.Interpreter/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir cmake-build-debug
-
-gcc -lubsan -o "main.o" -c "main.c" -Wall -Wextra -Werror -fsanitize=address -fsanitize=undefined
-
-gcc -lubsan -o "cmake-build-debug/PicoDotNet_Interpreter" *.o  -Wall -Wextra -Werror -fsanitize=address -fsanitize=undefined
-
-rm "main.o"
-./cmake-build-debug/PicoDotNet_Interpreter
