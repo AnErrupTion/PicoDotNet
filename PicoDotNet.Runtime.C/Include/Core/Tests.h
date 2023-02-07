fe61e731ff665a41fc9a70fa4d0197e4e5eb4397
AnErrupTion <anerruption@disroot.org>
2023-02-07 16:29:55 +0100

    Remove old stuff

--- PicoDotNet.Runtime.C/Include/Core/Tests.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Lib/Types.h>
-
-void PICO_SysTestHeap(size_t iters);
-void PICO_SysTestRAMFS(size_t iters);
-void PICO_SysTestThreading(size_t iters);
--- PicoDotNet.Runtime.C/Source/Boot/Main.c
+++
@@ -1,8 +0,0 @@
-#include <Core/Common.h>
-
-void main(PICO_Multiboot* mbp)
-{
-    PICO_KernelBoot(mbp);
-    PICO_ToggleScheduler(true);
-    PICO_KernelRun();
-}
